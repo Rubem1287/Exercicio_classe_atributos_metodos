11e626daac191569ef6659c7f68685aebbaab103
Rubem Silva <rubem.edu@outlook.com>
2021-02-27 17:09:07 -0300

    Add project files.

---
+++ Exercicio_classe_atributos_metodos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_classe_atributos_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e altura do retangulo : ");
+           retangulo.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = "+retangulo.Area(retangulo.Largura, retangulo.Altura).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro = "+retangulo.Perimetro(retangulo.Largura, retangulo.Altura).ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = "+retangulo.Diagonal(retangulo.Largura, retangulo.Altura).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicio_classe_atributos_metodos/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_classe_atributos_metodos
+{
+    class Retangulo
+    {
+
+        public double Largura;
+        public double Altura;
+
+
+        public double Area(double largura,double altura)
+        {
+            return (largura*altura);
+        }
+
+        public double Perimetro(double largura, double altura)
+        {
+            return ((largura + altura) * 2);
+        }
+
+        public double Diagonal(double largura, double altura)
+        {
+            return (Math.Sqrt(Math.Pow(largura,2) + Math.Pow(altura,2)));
+        }
+
+    }
+}
